de0ba73775b7e6d1ed77623ca8eb6e8464ed5d49
PhillipHenry <PhillHenry@gmail.com>
2024-05-28 17:58:18 +0100

    Guff cleaned.

--- main/src/my_merge_sort.rs
+++ main/src/my_merge_sort.rs
@@ -1,6 +1,6 @@
 use std::vec::Vec;
 
-pub fn sort<T: Clone + Ord + Copy + ToString + std::fmt::Debug>(xs: Vec<T>) -> Vec<T> {
+pub fn sort<T: Clone + Ord + Copy>(xs: Vec<T>) -> Vec<T> {
     if xs.len() <= 1 {
         return xs
     } else if xs.len() == 2 {
@@ -15,16 +15,12 @@ pub fn sort<T: Clone + Ord + Copy + ToString + std::fmt::Debug>(xs: Vec<T>) -> V
         let (first, second) = xs.split_at(mid);
         let vec0 = first.to_vec();
         let vec1 = second.to_vec();
-        println!("mid = {}, szie = {}, first = {}, second = {}", mid, size,
-                 first.iter().map(|x| x.to_string()).collect::<Vec<String>>().join(", "),
-                 second.iter().map(|x| x.to_string()).collect::<Vec<String>>().join(", "));
         let x0: Vec<T> = sort(vec0);
         let x1: Vec<T> = sort(vec1);
         let mut sorted: Vec<T> = vec![xs[0]; size];
         let mut i = 0;
         let mut j = 0;
         let mut index = 0;
-        println!("x0 = {:?}, x1 = {:?}, sorted = {:?}", x0, x1, sorted);
         while index < size {
             if i < x0.len() && (j == x1.len() || x0[i] < x1[j]) {
                 sorted[index] = x0[i];
