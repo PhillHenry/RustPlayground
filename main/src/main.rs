b9fdf07adb986341ec5dbd4f9e5ab79fd7e25a72
PhillipHenry <PhillHenry@gmail.com>
2024-06-01 10:35:53 +0100

    Comments

--- main/src/main.rs
+++ main/src/main.rs
@@ -5,15 +5,21 @@ use std::thread;
 fn main() {
     let mut s1 = String::from("hello");
 
-    let s_mutated = main::mutate(&mut s1);
+    let mut s_mutated = main::mutate(&mut s1);
     // this is allowed IN THIS ORDER:
     println!("s_mutated = {}", s_mutated);
+    s_mutated.push_str("XXX"); // allowed as we have not reused s1 yet
+
     println!("s1 = {}", s1); // s1 has been mutated!
-    // but if the above 2 lines were replaced with:
-    //println!("s_mutated = {}, s1 = {}", s_mutated, s1);
+    // println!("s_mutated = {}", s_mutated); <-- this would fail too. Seems that Rust is very pragmatic and if a violation doesn't cause a problem, allows it
+    // but if the above 2 uncommented lines were replaced with:
+    // println!("s_mutated = {}, s1 = {}", s_mutated, s1);
     // it would not be allowed!
     // The reason is actually simple. Once s_mutated has been 'spent' there are no longer any
     // mutable references to s1, therefore s1 can be used once more.
+
+    // s_mutated.push_str("XXX"); // <-- not allowed as we've already used s1 again
+    s1.push_str(" (polite interruption) ");
 
     let len = main::mutate_length(&mut s1);
     println!("The length of '{}' is {}", s1, len); // s1 is still valid here
@@ -42,11 +48,11 @@ mod main {
 
     pub fn mutate_in_own_thread(mut s: String) -> JoinHandle<String> {
         //s.push_str("mutant");
-        let handle = thread::spawn(move || {
+        let handle = thread::spawn( || { // 'move' appears to be optional...
             s.push_str("mutant");
             return s
         });
-        // println!("s = {}", s); // s has now been lost into the ether
+        // println!("s = {}", s); // "move occurs because `s` has type `String`, which does not implement the `Copy` trait"
         return handle
     }
 }
