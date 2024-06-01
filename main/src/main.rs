f5209e40626374f4b70b9f44f0da5846537130a2
PhillipHenry <PhillHenry@gmail.com>
2024-06-01 09:15:46 +0100

    Simple function calling

--- main/src/main.rs
+++ main/src/main.rs
@@ -1,5 +1,17 @@
 mod my_merge_sort;
+use std::sync::{Arc, Mutex};
+use std::thread;
 
 fn main() {
-    println!("Hello, world!");
+    let mut s1 = String::from("hello");
+    let len = main::calculate_length(&mut s1); // Borrowing s1 immutably
+    println!("The length of '{}' is {}", s1, len); // s1 is still valid here
+}
+
+mod main {
+
+    pub fn calculate_length(s: &mut String) -> usize {
+        s.push_str("mutant");
+        s.len()
+    }
 }
